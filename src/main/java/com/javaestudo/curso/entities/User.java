c2e2d68cb833712ed1f74abebff006491f2ac93e
Gabriel Silva <gabrieldasilvamonteiro2@gmail.com>
2026-02-26 19:26:51 -0300

    h2 database, test profile, JPA

--- src/main/java/com/javaestudo/curso/entities/User.java
+++ src/main/java/com/javaestudo/curso/entities/User.java
@@ -1,13 +1,20 @@
 package com.javaestudo.curso.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "users")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String phone;
