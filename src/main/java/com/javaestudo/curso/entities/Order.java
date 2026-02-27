f59d0698a7eae32df8a67a19b7380287c11e7fa6
Gabriel Silva <gabrieldasilvamonteiro2@gmail.com>
2026-02-27 12:22:12 -0300

    OrderStatus enum

--- src/main/java/com/javaestudo/curso/entities/Order.java
+++ src/main/java/com/javaestudo/curso/entities/Order.java
@@ -2,6 +2,7 @@ package com.javaestudo.curso.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.javaestudo.curso.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -15,10 +16,12 @@ public class Order implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
 
     @JsonFormat(shape =  JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd 'T' HH:mm:ss 'Z'", timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
 
     @JsonIgnore
     @ManyToOne
@@ -27,18 +30,19 @@ public class Order implements Serializable {
 
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
-        Id = id;
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        setOrderStatus(orderStatus);
         this.moment = moment;
         this.client = client;
     }
 
     public Long getId() {
-        return Id;
+        return id;
     }
 
     public void setId(Long id) {
-        Id = id;
+        id = id;
     }
 
     public Instant getMoment() {
@@ -47,6 +51,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus) ;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
@@ -61,11 +75,11 @@ public class Order implements Serializable {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Order order = (Order) o;
-        return Objects.equals(Id, order.Id);
+        return Objects.equals(id, order.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(Id);
+        return Objects.hashCode(id);
     }
 }
